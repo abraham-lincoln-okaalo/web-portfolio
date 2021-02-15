d5a92ed9eb2e62b4029027b3abb67e0551084f8b
okaalo <lincolnokaalo@gmail.com>
2021-02-15 12:49:33 +0300

    Added preloader's html, css, js.

--- js/main.js
+++ js/main.js
@@ -299,3 +299,11 @@ function bodyScrollingToggle(){
         }
     });
 })();
+
+window.addEventListener("load", ()=>{
+    // preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(()=>{
+        document.querySelector("preloader").style.display="none";
+    }, 600)
+})
